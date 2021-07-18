ea4dedee48368bcc7a61fa73addd7870bdf0f7d5
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-18 23:21:18 +0430

    Get a four-item list of products (Lazy loading)
    Instead of loading the entire table and rendering it to the
    user in one go as in bulk loading, the concept of lazy
    loading assists in loading only the required section and
    delays the remaining, until it is needed by the user. Lazy
    loading causes improved performance and Less traffic load
    for the host.

--- get_product_list.php
+++ get_product_list.php
@@ -1,10 +1,20 @@
 <?php
 
 	include 'setup/db_connection.php';
-    $mysqli = OpenConnection();
+    $mysqli = openConnection();
 
 	// the variable in which we store the id number of the currently selected data item
     $prdc_id = isset($_POST['product_id']) ? $_POST['product_id'] : 'Failed to set GET superglobals.';
 
+    $query = "SELECT * FROM product WHERE product_id BETWEEN ($prdc_id+1) AND ($prdc_id+4)";
+    $result = $mysqli -> query($query);
+
+    while($row = $result -> fetch_assoc()) {
+        $array[] = $row;
+    }
+
+    echo json_encode($array);
+
+    $result -> free_result();
     $mysqli -> close();
 ?>
