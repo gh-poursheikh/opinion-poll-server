a369dc588c1488f48a65c8b89e02b26b918ed102
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-18 21:33:47 +0430

    Check if the client app provides both parameters

--- login.php
+++ login.php
@@ -1,7 +1,21 @@
 <?php
 
 	include 'setup/db_connection.php';
-	$mysqli = openConnection();
+	$mysqli = OpenConnection();
+
+	if(isset($_POST["username"], $_POST["password"])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		if(!empty($username) && !empty($password)) {
+			echo ("Both fields provided!\n");		
+		} else {
+			echo ("You must fill both fields.\n");
+		}
+
+	} else {
+		echo "Failed to set POST superglobals.\n";
+	}
 	$mysqli -> close();
 
 ?>
