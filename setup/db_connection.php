c16ad92d8f105a138974743061fd19425817a3bd
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-18 21:27:01 +0430

    Add character encoding and output type
    Set the output to support non-ASCII characters encoding and
    to be in JSON format so that the client can decode and use
    it.

--- setup/db_connection.php
+++ setup/db_connection.php
@@ -2,6 +2,8 @@
 
 	require_once 'db_connection_params.php';
 
+	header('Content-Type: application/json; charset=utf-8');
+
 	function openConnection()
  	{
  		$mysqli = new mysqli(dbhost, dbuser, dbpass, dbname) or die("Connection failed: " . $mysqli -> connect_error);
