305dbc8edfda3fdbe9f899d5c508eb2fd7e1d11a
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-18 21:36:26 +0430

    Do the login query

--- login.php
+++ login.php
@@ -8,7 +8,13 @@
 		$password = $_POST['password'];
 		
 		if(!empty($username) && !empty($password)) {
-			echo ("Both fields provided!\n");		
+			$query = "SELECT * FROM person WHERE username = '$username' AND password = '$password'";
+			$result = $mysqli -> query($query);
+			
+			$json = $result -> fetch_assoc();
+			echo json_encode($json);
+
+			$result -> free_result();
 		} else {
 			echo ("You must fill both fields.\n");
 		}
@@ -16,6 +22,7 @@
 	} else {
 		echo "Failed to set POST superglobals.\n";
 	}
+
 	$mysqli -> close();
 
 ?>
