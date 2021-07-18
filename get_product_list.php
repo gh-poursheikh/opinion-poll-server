dfce0a1c0da6b0a247b2b5cf71ced85d90aa48e8
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-18 21:57:57 +0430

    Check if the client app provides the parameter

--- get_product_list.php
+++ get_product_list.php
@@ -1,7 +1,10 @@
 <?php
 
 	include 'setup/db_connection.php';
-    $mysqli = openConnection();
-    $mysqli -> close();
+    $mysqli = OpenConnection();
 
+	// the variable in which we store the id number of the currently selected data item
+    $prdc_id = isset($_POST['product_id']) ? $_POST['product_id'] : 'Failed to set GET superglobals.';
+
+    $mysqli -> close();
 ?>
