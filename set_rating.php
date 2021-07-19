ac5f4a606dbc733ef4412e3414c30b0366dafa8f
Ghassem Poursheikh <gh.poursheikh@gmail.com>
2021-07-19 16:26:01 +0430

    Check if the client app provides user ratings

--- set_rating.php
+++ set_rating.php
@@ -2,6 +2,9 @@
 
 	include 'setup/db_connection.php';
 	$mysqli = openConnection();
+
+    $payload = isset($_POST['params']) ? $_POST['params'] : 'Failed to set POST superglobals.';
+
 	$mysqli -> close();
 
 ?>
